43337b050370c7acb11274d0091709b5710d560b
brunogabriel <bruno.gsantos89@gmail.com>
2016-08-08 23:17:02 -0300

    Added structure to enter username, validator on form and click by using ButterKnife

--- AndroidSocketChatRoom/app/src/main/java/br/com/socketchatroomapp/activities/EnterActivity.java
+++ AndroidSocketChatRoom/app/src/main/java/br/com/socketchatroomapp/activities/EnterActivity.java
@@ -1,23 +1,99 @@
 package br.com.socketchatroomapp.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+
+import java.util.List;
 
 import br.com.socketchatroomapp.R;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by brunogabriel on 8/8/16.
  */
-public class EnterActivity extends BaseActivity {
+public class EnterActivity extends BaseActivity implements Validator.ValidationListener{
+
+    @BindView(R.id.toolbar)
+    protected Toolbar mToolbar;
+
+    @NotEmpty(message = "This field should be filled")
+    @BindView(R.id.usernameEditText)
+    protected AppCompatEditText mUsernameEditText;
+
+    @BindString(R.string.app_name)
+    protected String mScreenTitle;
+
+    protected Validator mValidator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+        ButterKnife.bind(this);
         initUI();
     }
 
     @Override
     public void initUI() {
+        setupToolBar(mToolbar, mScreenTitle, null, false, false);
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
+        mUsernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String mResult = editable.toString().replace(" ", "");
+                if (!editable.toString().equals(mResult)) {
+                    mUsernameEditText.setText(mResult);
+                    mUsernameEditText.setSelection(mResult.length());
+                }
+            }
+        });
+    }
+
+    @OnClick(R.id.enterButton)
+    protected void onClickEnterButton() {
+        mValidator.validate();
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        String mUsername = mUsernameEditText.getText().toString().trim();
+
+        if (isNetworkAvailable()) {
+            // TODO: validate if in this room has the same user name
+        } else {
+            // TODO: Show error Dialog
+        }
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError mError : errors) {
+            String mErrorMessage = mError.getCollatedErrorMessage(this);
+            View mView = mError.getView();
+            if (mView instanceof AppCompatEditText)
+                ((AppCompatEditText) mView).setError(mErrorMessage);
+
+        }
     }
 }
